6f33dcac7b22ac2eaadd4272114950f5016a4baf
Lucas Gomes <lucasgomesbr@gmail.com>
2021-03-18 00:13:24 -0300

    fisrt commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let express = require('express');
+var load = require('express-load');
+
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+var error = require('./middlewares/error');
+
+app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(cookieParser('nodeEventos'));
+app.use(expressSession());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
+
+//middlewares
+app.use(error.notFound);
+app.use(error.serverError);
+
+app.listen(4000, function () {
+  console.log("App no ar.");
+});
---
+++ controllers/eventos.js
@@ -0,0 +1,34 @@
+module.exports = function (app) {
+  var EventosController = {
+    menu: function (request, response) {
+      var usuario = request.session.usuario,
+        params = { usuario: usuario };
+      response.render('eventos/menu', params);
+    },
+
+    cadastroUsuario: function (request, response) {
+      var usuario = request.session.usuario,
+        params = { usuario: usuario };
+      response.render('eventos/cadUsuario', params);
+    },
+
+    cadastroEvento: function (request, response) {
+      var usuario = request.session.usuario,
+        params = { usuario: usuario };
+      response.render('eventos/cadEvento', params);
+    },
+
+    listaEventos: function (request, response) {
+      var usuario = request.session.usuario,
+        params = { usuario: usuario };
+      response.render('eventos/listaEventos', params);
+    },
+
+    novoEvento: function (request, response) {
+      //código a ser implementado
+      response.redirect('/menu');
+    }
+  };
+
+  return EventosController;
+};
